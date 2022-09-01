352ed82f45e95beecf210284b8c4d12cce4c1d57
Sarah Carter <carters@transblue.org>
2022-09-01 10:34:06 -0700

    created a reusable PageLayout component with mterial ui app bar

---
+++ src/components/pageLayout/pageLayout.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { MenuItem, Menu } from "@mui/material";
+
+function PageLayout() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => setAnchorEl(null);
+
+    return(
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    {/* <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        5 Minute Friday
+                    </Typography>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default PageLayout;
--- src/pages/thumbnails/thumbnails.js
+++ src/pages/thumbnails/thumbnails.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
+import PageLayout from "../../components/pageLayout/pageLayout";
 
 function Thumbnails() {
     const [videos, setVideos] = useState([]);
 
     useEffect(() => {
-        
+
     }, [])
 
     return(
-        <></>
+        <PageLayout>
+            
+        </PageLayout>
     )
 }
 
